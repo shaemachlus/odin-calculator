b294aa0a1cba62bf01ddeb4ab730e5f2304846cd
Shae Machlus <shaemachlus@gmail.com>
2022-03-18 10:32:29 -0400

    Add keyboard support and prevent display overflow

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// to-do
+// add scientific notation for large calculations
+// highlight the operation being used
+// add keyboard support
+
+const COLOR_OP_SELECTED = "#fff";
+const COLOR_OP_UNSELECTED = "#000";
+
+const opButtons = document.querySelectorAll(".btn-op");
+const numButtons = document.querySelectorAll(".btn-num");
+const display = document.querySelector(".display");
+const clearButton = document.querySelector(".btn-clear");
+const equalsButton = document.querySelector(".btn-equals");
+const decimalButton = document.querySelector(".btn-decimal");
+const delButton = document.querySelector(".btn-del");
+
+let isClear = true; // should the next number clear the display
+let firstNum;
+let secondNum;
+let currentOperator;
+
 function add(num1, num2) {
   return +num1 + +num2;
 }
@@ -11,6 +32,9 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+  if (+num2 === 0) {
+    return "error";
+  }
   return num1 / num2;
 }
 
@@ -33,53 +57,32 @@ function operate(num1, num2, opString) {
       alert("Error: operation missing");
       break;
   }
-  return Math.round(optr(num1, num2) * 1e6) / 1e6;
+  let res = Math.round(optr(num1, num2) * 1e6) / 1e6;
+  if (res.toString().length > 8) {
+    return "Overflow";
+  }
+  return res;
 }
 
-const opButtons = document.querySelectorAll(".btn-op");
-const numButtons = document.querySelectorAll(".btn-num");
-const display = document.querySelector(".display");
-const clearButton = document.querySelector(".btn-clear");
-const equalsButton = document.querySelector(".btn-equals");
-const decimalButton = document.querySelector(".btn-decimal");
-const delButton = document.querySelector(".btn-del");
+function convertEventToString(stringOrEvent) {
+  if (typeof stringOrEvent === "string") {
+    return stringOrEvent;
+  } else {
+    return stringOrEvent.target.textContent;
+  }
+}
 
-let isClear = true; // should the next number clear the display
-let firstNum;
-let secondNum;
-let op;
+function enterNum(stringOrEvent) {
+  let num = convertEventToString(stringOrEvent);
+  if (isClear) {
+    display.textContent = num;
+    isClear = false;
+  } else if (display.textContent.length < 8) {
+    display.textContent += num;
+  }
+}
 
-numButtons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    if (isClear) {
-      display.textContent = btn.textContent;
-      isClear = false;
-    } else if (display.textContent.length < 8) {
-      display.textContent += btn.textContent;
-    }
-  });
-});
-
-opButtons.forEach((opBtn) => {
-  opBtn.addEventListener("click", () => {
-    if (op) {
-      secondNum = display.textContent;
-      display.textContent = operate(firstNum, secondNum, op);
-    }
-    firstNum = display.textContent;
-    op = opBtn.textContent;
-    isClear = true;
-  });
-});
-
-equalsButton.addEventListener("click", () => {
-  secondNum = display.textContent;
-  display.textContent = operate(firstNum, secondNum, op);
-  isClear = true;
-  op = null;
-});
-
-decimalButton.addEventListener("click", () => {
+function enterDecimal() {
   if (!display.textContent.includes(".") || isClear) {
     if (isClear) {
       display.textContent = "0.";
@@ -88,21 +91,62 @@ decimalButton.addEventListener("click", () => {
       display.textContent += ".";
     }
   }
-});
+}
 
-clearButton.addEventListener("click", () => {
-  display.textContent = 0;
-  firstNum = null;
-  secondNum = null;
-  op = null;
+function enterOperator(stringOrEvent) {
+  let newOperator = convertEventToString(stringOrEvent);
+  if (currentOperator) {
+    secondNum = display.textContent;
+    display.textContent = operate(firstNum, secondNum, currentOperator);
+  }
+  firstNum = display.textContent;
+  currentOperator = newOperator;
+  // opBtn.classList.toggle("op-selected");
   isClear = true;
-});
+}
 
-delButton.addEventListener("click", () => {
+function enterEquals() {
+  secondNum = display.textContent;
+  display.textContent = operate(firstNum, secondNum, currentOperator);
+  isClear = true;
+  currentOperator = null;
+}
+
+function enterDel() {
   if (display.textContent.length > 1) {
     display.textContent = display.textContent.slice(0, -1);
   } else {
     display.textContent = 0;
     isClear = true;
   }
+}
+
+function enterClear() {
+  display.textContent = 0;
+  firstNum = null;
+  secondNum = null;
+  currentOperator = null;
+  isClear = true;
+}
+
+window.addEventListener("keydown", (e) => {
+  if (!isNaN(e.key)) {
+    enterNum(e.key);
+  } else if (e.key === ".") {
+    enterDecimal();
+  } else if (e.key === "x" || e.key === "/" || e.key === "-" || e.key === "+") {
+    enterOperator(e.key);
+  } else if (e.key === "Enter" || e.key === "=") {
+    // "enter" button does not always work as expected
+    enterEquals();
+  } else if (e.key === "Backspace") {
+    enterDel();
+  }
 });
+
+numButtons.forEach((btn) => btn.addEventListener("click", enterNum));
+opButtons.forEach((opBtn) => opBtn.addEventListener("click", enterOperator));
+decimalButton.addEventListener("click", enterDecimal);
+equalsButton.addEventListener("click", enterEquals);
+clearButton.addEventListener("click", enterClear);
+delButton.addEventListener("click", enterDel);
